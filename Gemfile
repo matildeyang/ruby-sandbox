a649c090e520a22310b19043cb8aec686809252a
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-10 22:09:20 +0000

    adding gemfile

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# pulling in other gems 
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
--- howdy.rb
+++ howdy.rb
@@ -4,3 +4,12 @@ pp my_string
 # path to get to goodbye.rb 
 # if run howdy.ruby will also run goodbye 
 require "./goodbye.rb"
+
+# load this so can access what other people have written in Ruby
+require "active_support/all"
+
+1.ordinalize    # => "1st"
+2.ordinalize    # => "2nd"
+
+"table".pluralize     # => "tables"
+"rubies".singularize    # => "ruby"
